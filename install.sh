d44f6acf9700ac87e3cfd313474f2aea3a7287be
Jeremiah Boby <mail@jeremiahboby.me>
2020-08-08 20:55:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,17 @@
-echo 0
+#!/usr/bin/env sh
+
+[[ "$UID" != "0" ]] && echo "Run the RACTF install script as root." && exit 1
+
+LOCATION=$(
+  curl -s https://api.github.com/repos/ractf/install/releases/latest \
+    | grep -Po '(?<="browser_download_url": ")(.+)' \
+    | sed 's/"//' \
+    | head -1
+)
+
+FILE=$(mktemp)
+
+curl -SsL $LOCATION > $FILE
+chmod +x $FILE
+
+$FILE
